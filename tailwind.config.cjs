ce8aa64c6dbc9db91159ff7655dd6adb29d7df07
Elijah Walker <elijahtruthwalker05@gmail.com>
2025-05-28 02:20:08 -0500

    Fix config files to use .cjs extension

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,18 +11,10 @@ module.exports = {
         sans: ['EB Garamond', 'serif'],
       },
       colors: {
-        baby_powder: {
-          DEFAULT: '#fbfef9',
-          100: '#2b560f',
-          200: '#57ad1d',
-          300: '#89e14e',
-          400: '#c2f0a4',
-          500: '#fbfef9',
-          600: '#fdfefc',
-          700: '#fdfffc',
-          800: '#fefffd',
-          900: '#fefffe'
-        },
+        baby_powder: '#fbfef9',
+        eerie_black: '#1D1D1D',
+        yinmn_blue: '#30577a',
+        hunter_green: '#435b35',
         black: {
           DEFAULT: '#000000',
           100: '#000000',
@@ -35,18 +27,6 @@ module.exports = {
           800: '#999999',
           900: '#cccccc'
         },
-        yinmn_blue: {
-          DEFAULT: '#30577a',
-          100: '#091118',
-          200: '#132330',
-          300: '#1c3449',
-          400: '#264561',
-          500: '#30577a',
-          600: '#427aaa',
-          700: '#6b9bc6',
-          800: '#9dbdd9',
-          900: '#cedeec'
-        },
         falu_red: {
           DEFAULT: '#7b2d26',
           100: '#190908',
@@ -58,18 +38,6 @@ module.exports = {
           700: '#ce6960',
           800: '#de9b95',
           900: '#efcdca'
-        },
-        hunter_green: {
-          DEFAULT: '#435b35',
-          100: '#0d120b',
-          200: '#1b2415',
-          300: '#283620',
-          400: '#35482a',
-          500: '#435b35',
-          600: '#658850',
-          700: '#89ad74',
-          800: '#b0c9a2',
-          900: '#d8e4d1'
         },
         dark: {
           50: '#f6f6f7',
